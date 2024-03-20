05b29cd46a0a0cf7e401e9e01be6f402797ad443
Hendri Wahyu Perdana <hendriwp123@gmail.com>
2024-03-20 13:02:41 +0700

    fix: fontsize on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
       className="mx-auto bottom-0 w-full p-4 border-t border-gray-200 shadow flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600 bg-cyan-700 "
       style={{ placeContent: "center" }}
     >
-      <span className="text-sm text-white dark:text-gray-400">
+      <span className="text-xl text-white dark:text-gray-400">
         © 2024{" "}
         <a
           href="https://www.instagram.com/hendri.whyu/"
